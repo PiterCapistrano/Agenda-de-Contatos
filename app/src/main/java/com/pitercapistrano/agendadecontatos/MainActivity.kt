658ae23e64c5fd931afd8048e7530d22be509274
Piter Capistrano <pitercapistrano@gmail.com>
2024-10-03 20:08:18 -0300

    Recuperando dados da tabela e inserindo na tela principal

--- app/src/main/java/com/pitercapistrano/agendadecontatos/MainActivity.kt
+++ app/src/main/java/com/pitercapistrano/agendadecontatos/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pitercapistrano.agendadecontatos
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
@@ -7,12 +8,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pitercapistrano.agendadecontatos.adapter.ContatoAdapter
+import com.pitercapistrano.agendadecontatos.dao.UsuarioDao
 import com.pitercapistrano.agendadecontatos.databinding.ActivityMainBinding
+import com.pitercapistrano.agendadecontatos.model.Usuario
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var usuarioDao: UsuarioDao
+    private lateinit var contatoAdapter: ContatoAdapter
+    private val _listaUsuarios = MutableLiveData<MutableList<Usuario>>()
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,6 +37,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            getContatos()
+
+            withContext(Dispatchers.Main){
+
+                _listaUsuarios.observe(this@MainActivity){ listaUsuarios ->
+                    val recyclerViewContatos = binding.recyclerViewContatos
+                    recyclerViewContatos.layoutManager = LinearLayoutManager(this@MainActivity)
+                    recyclerViewContatos.setHasFixedSize(true)
+                    contatoAdapter = ContatoAdapter(this@MainActivity, listaUsuarios)
+                    recyclerViewContatos.adapter = contatoAdapter
+                    contatoAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+
         // Configura a cor da barra de status como roxa
         window.statusBarColor = Color.parseColor("#673AB7") // Altera a cor da barra de status para roxa
         window.decorView.systemUiVisibility = 0 // Define a visibilidade do sistema UI para o padrão
@@ -32,5 +63,33 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onResume() {
+        super.onResume()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            getContatos()
+
+            withContext(Dispatchers.Main){
+
+                _listaUsuarios.observe(this@MainActivity){ listaUsuarios ->
+                    val recyclerViewContatos = binding.recyclerViewContatos
+                    recyclerViewContatos.layoutManager = LinearLayoutManager(this@MainActivity)
+                    recyclerViewContatos.setHasFixedSize(true)
+                    contatoAdapter = ContatoAdapter(this@MainActivity, listaUsuarios)
+                    recyclerViewContatos.adapter = contatoAdapter
+                    contatoAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+    private fun getContatos(){
+        usuarioDao = AppDatabase.getInstance(this).usuarioDao()
+        val listaUsuarios: MutableList<Usuario> = usuarioDao.get()
+
+        _listaUsuarios.postValue(listaUsuarios)
     }
 }
--- app/src/main/java/com/pitercapistrano/agendadecontatos/dao/UsuarioDao.kt
+++ app/src/main/java/com/pitercapistrano/agendadecontatos/dao/UsuarioDao.kt
@@ -2,6 +2,7 @@ package com.pitercapistrano.agendadecontatos.dao // Declara o pacote onde a clas
 
 import androidx.room.Dao // Importa a anotação Dao do Room, usada para definir a interface de acesso ao banco de dados
 import androidx.room.Insert // Importa a anotação Insert para inserir dados no banco de dados
+import androidx.room.Query
 import com.pitercapistrano.agendadecontatos.model.Usuario // Importa a classe Usuario, que representa a entidade do banco de dados
 
 @Dao // Anota a interface como um DAO (Data Access Object) para a entidade Usuario
@@ -9,4 +10,8 @@ interface UsuarioDao { // Declara a interface UsuarioDao para manipular dados no
 
     @Insert // Anota o método como uma operação de inserção de dados no banco
     fun inserir(listaUsuarios: MutableList<Usuario>) // Método que insere uma lista mutável de objetos Usuario no banco de dados
+
+    @Query("SELECT * FROM tabela_usuarios ORDER BY nome ASC")
+    fun get(): MutableList<Usuario>
+
 }
